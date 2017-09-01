71e2357d43e0c2bd8bf336d3e3591574ec6a0723
Tu Ngo <tungo8810@gmail.com>
2017-09-01 10:20:49 -0700

    Add ModelAdmin classes

--- catalog/models.py
+++ catalog/models.py
@@ -42,6 +42,14 @@ class Book(models.Model):
         """
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+
+    display_genre.short_description = 'Genre'
+
 
 import uuid # Required for unique book instances
 
